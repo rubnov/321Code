1046897b038629a94465eaeb6ff32badca9b01cd
Rotem Rubnov <rotem@gopublicapp.com>
2013-07-07 10:38:10 +0200

    Initial Commit

---
+++ 321code/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  321code
+//
+//  Created by Rotem Backup on 7/7/2013.
+//  Copyright 321Code 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ 321code/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  321code
+//
+//  Created by Rotem Backup on 7/7/2013.
+//  Copyright 321Code 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
